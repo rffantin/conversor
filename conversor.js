1bcac35698e870e92b5720cd63f0f851a8ec4766
rffantin <rffantin@gmail.com>
2023-06-25 19:56:53 -0300

    iniciado o javascript da pagina

---
+++ conversor.js
@@ -0,0 +1,12 @@
+const unidades = {
+    comprimento: ['metros', 'centímetros', 'polegadas'],
+    peso: ['quilogramas', 'gramas', 'libras'],
+    temperatura: ['Celsius', 'Fahrenheit', "Kelvin"]
+}
+
+const categoriaSelect = document.getElementById('categoria')
+const unidadeOrigemSelect = document.getElementById('unidadeOrigem')
+const unidadeDestinoSelect = document.getElementById('unidadeDestino')
+const valorInput = document.getElementById('valor')
+const resultadoInput = document.getElementById('resultado')
+
